e03a87c589e786912cea3851f7b791c0f5827024
wessam developer <161109058+Wessam222222222@users.noreply.github.com>
2025-03-26 11:17:10 +0400

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://wessamahmedsamirahmed:tPHeVa9P9G4EKTT9@cluster0.7ohw8f8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-.then(() =>{
-    consol.log('connected to DB')
-})
-.catch(() =>{
-    console.logzz('Unable to connected to BD')
-})    
